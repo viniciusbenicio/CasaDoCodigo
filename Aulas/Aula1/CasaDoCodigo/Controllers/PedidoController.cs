8ffb76c46954c87917af6531acf4ef6baca6a967
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-27 21:39:38 -0300

    Adicionando Layout Pronto

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+    }
+}
