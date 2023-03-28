832e40965610989ad080f9cb09b4d3105423e3c9
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-27 21:45:40 -0300

    Removendo classes e arquivos que não serão necessários

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -9,5 +9,20 @@ namespace CasaDoCodigo.Controllers
             return View();
         }
 
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+
     }
 }
