8466d13cc0778a8fdca955e96eea1ac8a0b8c470
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-04-04 19:27:54 -0300

    Organizando Código javascript com Classe

---
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/Carrinho.js
@@ -0,0 +1,37 @@
+﻿class Carrinho {
+    clickAddProduto(btn) {
+        let data = this.getData(btn);
+        data.Quantidade++;
+        this.postQuantidade(data);
+       
+    }
+
+    clickRemoveProduto(btn) {
+        let data = this.getData(btn);
+        data.Quantidade--;
+        this.postQuantidade(data);
+    }
+
+    getData(elemento) {
+        var linhaDoItem = $(elemento).parents('[item-id]');
+        var itemId = $(linhaDoItem).attr('item-id');
+        var novaQtde = $(linhaDoItem).find('input').val();
+
+        return data = {
+            Id: itemId,
+            Quantidade: novaQtde
+        };
+    }
+
+    postQuantidade(data) {
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        });
+    }
+}
+
+
+var carrinho = new Carrinho();
