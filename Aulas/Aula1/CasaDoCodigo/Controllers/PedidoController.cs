d6d26a6f1fe03019bf1eb19efe1ab251260e32cc
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-04-10 19:39:50 -0300

    Injetando o cadastro na view e redirecionando se necessário

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -38,7 +38,12 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Cadastro()
         {
-            return View();
+            var pedido = pedidoRepository.GetPedido();
+
+            if(pedido == null)
+                return RedirectToAction("Carrossel");
+
+            return View(pedido.Cadastro);
         }
 
         public IActionResult Resumo()
